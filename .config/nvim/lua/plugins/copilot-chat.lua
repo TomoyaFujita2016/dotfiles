9d9b3092bb1541e52881236664b0ce6f6dbc290b
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2025-03-07 23:01:49 +0900

    copilot chat追加

---
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      window = {
+        layout = "vertical", -- 'float' | 'bottom' | 'top' | 'right' | 'left'
+        width = 0.4, -- width of the chat window when layout is 'float', 'right' or 'left'
+        height = 1.0, -- height of the chat window when layout is 'float', 'top' or 'bottom'
+      },
+
+      prompts = {
+        SpeakInJapanese = {
+          system_prompt = "ユーザーの質問には日本語で答えること。",
+        },
+      },
+    },
+    keys = {
+      { "<leader>cp", "<cmd>CopilotChatToggle<CR>", desc = "CopilotChat - 通常チャット" },
+    },
+  },
+}
