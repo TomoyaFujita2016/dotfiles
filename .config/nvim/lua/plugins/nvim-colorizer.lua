63e38f616ea8d5acbe79aa31bcf3157e1f34eab8
t-fujita24 <fujita.t.2016c@gmail.com>
2025-12-01 22:33:33 +0900

    chore: プラグインの更新とnvim-colorizerの設定変更
    - lazy-lock.jsonで複数のプラグインを最新版に更新
    - nvim-colorizerをnorcalli版からNvChad版に移行
    - nvim-colorizerの設定を新しいAPI形式に対応
      - filetypesとuser_default_optionsの構造に変更
      - 機能設定をuser_default_options内に移動

--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,36 +1,39 @@
 return {
   {
-    "norcalli/nvim-colorizer.lua",
+    "NvChad/nvim-colorizer.lua",
     lazy = false,
     config = function()
       require("colorizer").setup({
-        "css",
-        "javascript",
-        "typescript",
-        "typescriptreact",
-        "javascriptreact",
-        "python",
-        "lua",
-        "vim",
-        "sh",
-        "bash",
-        "zsh",
-        "dockerfile",
-        "toml",
-        "yaml",
-        "json",
-        "markdown",
-        "html",
-        "xml",
-      }, {
-        RGB = true, -- #RGB形式のカラーコード
-        RRGGBB = true, -- #RRGGBB形式のカラーコード
-        names = true, -- カラーネーム (Red, Blueなど)
-        RRGGBBAA = true, -- #RRGGBBAA形式のカラーコード
-        rgb_fn = true, -- CSS形式のrgb()関数
-        hsl_fn = true, -- CSS形式のhsl()関数
-        css = true, -- CSS形式のカラー指定
-        css_fn = true, -- CSS関連の関数
+        filetypes = {
+          "css",
+          "javascript",
+          "typescript",
+          "typescriptreact",
+          "javascriptreact",
+          "python",
+          "lua",
+          "vim",
+          "sh",
+          "bash",
+          "zsh",
+          "dockerfile",
+          "toml",
+          "yaml",
+          "json",
+          "markdown",
+          "html",
+          "xml",
+        },
+        user_default_options = {
+          RGB = true, -- #RGB形式のカラーコード
+          RRGGBB = true, -- #RRGGBB形式のカラーコード
+          names = true, -- カラーネーム (Red, Blueなど)
+          RRGGBBAA = true, -- #RRGGBBAA形式のカラーコード
+          rgb_fn = true, -- CSS形式のrgb()関数
+          hsl_fn = true, -- CSS形式のhsl()関数
+          css = true, -- CSS形式のカラー指定
+          css_fn = true, -- CSS関連の関数
+        },
       })
     end,
   },
